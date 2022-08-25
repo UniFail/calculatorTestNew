8548277966ab240dc8f8043595d44739800d3291
UniFail <vika.lolkova@mail.ru>
2022-08-25 16:20:22 +0700

    HomeWork2.12

---
+++ calculator/src/main/java/com/calculatorcalculator/CalculatorService.java
@@ -0,0 +1,10 @@
+package com.calculatorcalculator;
+
+public interface CalculatorService {
+      String welcome();
+      int plus(Integer num1, Integer num2);
+      int minus(Integer num1, Integer num2);
+      int multiply(Integer num1, Integer num2);
+      int divide(Integer num1, Integer num2);
+
+}
